6256360a05d7c2146cfdc26bd2f3671d1b7b5881
Tiago Bohnenberger <tiagoboh10@gmail.com>
2021-06-08 20:12:59 -0300

    Many-to-many association with JoinTable

--- src/main/java/webservicesproject/repositories/OrderRepository.java
+++ src/main/java/webservicesproject/repositories/OrderRepository.java
@@ -2,7 +2,6 @@ package webservicesproject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import webservicesproject.entities.Order;
-import webservicesproject.entities.User;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
