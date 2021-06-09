9555f716b4ceebf12c8519739a0aad9080507107
Tiago Bohnenberger <tiagoboh10@gmail.com>
2021-06-09 11:28:53 -0300

    Exception handling - findById

--- src/main/java/webservicesproject/entities/Order.java
+++ src/main/java/webservicesproject/entities/Order.java
@@ -15,7 +15,6 @@ import java.util.Set;
 public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
